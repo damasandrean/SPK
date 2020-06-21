a2f5cc98e9d8bdc2e1cda7fdfbdb54d57f60fb3c
damasandrean <48828026+damasandrean@users.noreply.github.com>
2020-06-21 18:55:33 +0700

    Add files via upload

---
+++ electree.php
@@ -0,0 +1,48 @@
+<html>
+<title>SPK</title>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="#" class="navbar-brand">Sisitem Pendukung Keputusan</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a href="index.php" class="nav-item nav-link">Metode SAW</a>
+                <a href="#" class="nav-item nav-link active">Metode WP</a>
+                <a href="ahp.php" class="nav-item nav-link">Metode AHP</a>
+                <a href="electree.php" class="nav-item nav-link">Metode ELECTREE</a>
+            </div>
+        </div>
+    </nav>
+    <br>
+     <table class="table">
+        <thead>
+            <tr>
+                <th>Nama</th>
+                <th>Kelas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <th>DAMAS ANDREAN NUR WICAKSONO</th>
+            <th>DIV Teknik Informatika 3C</th>
+        </tbody>
+    </table>
+    <br>
+    <br>
+    <br>
+
+
+    <h5>Metode Electree</h5>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
